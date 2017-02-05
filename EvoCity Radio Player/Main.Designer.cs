ed635ca07d05e9190b2ba01e123f8a28d629c6bb
Hamza Akhtar <Hsk10@hotmail.co.uk>
2017-02-05 02:37:46 +0000

    Add project files.

---
+++ EvoCity Radio Player/Main.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace EvoCity_Radio_Player
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.msmMain = new MetroFramework.Components.MetroStyleManager();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusLabel = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msmMain
+            // 
+            this.msmMain.OwnerForm = this;
+            this.msmMain.Style = MetroFramework.MetroColorStyle.Orange;
+            this.msmMain.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.CustomBackground = false;
+            this.metroToggle1.DisplayStatus = true;
+            this.metroToggle1.FontSize = MetroFramework.MetroLinkSize.Small;
+            this.metroToggle1.FontWeight = MetroFramework.MetroLinkWeight.Regular;
+            this.metroToggle1.Location = new System.Drawing.Point(89, 77);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 18);
+            this.metroToggle1.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroToggle1.StyleManager = this.msmMain;
+            this.metroToggle1.TabIndex = 0;
+            this.metroToggle1.Text = "Off";
+            this.metroToggle1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroToggle1.UseStyleColors = false;
+            this.metroToggle1.UseVisualStyleBackColor = true;
+            this.metroToggle1.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(23, 77);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(35, 36);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.CustomBackground = false;
+            this.metroProgressSpinner1.Location = new System.Drawing.Point(244, 142);
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.Size = new System.Drawing.Size(22, 20);
+            this.metroProgressSpinner1.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroProgressSpinner1.StyleManager = this.msmMain;
+            this.metroProgressSpinner1.TabIndex = 6;
+            this.metroProgressSpinner1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.CustomBackground = false;
+            this.statusLabel.CustomForeColor = false;
+            this.statusLabel.FontSize = MetroFramework.MetroLabelSize.Medium;
+            this.statusLabel.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.statusLabel.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.statusLabel.Location = new System.Drawing.Point(89, 108);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(97, 19);
+            this.statusLabel.Style = MetroFramework.MetroColorStyle.Orange;
+            this.statusLabel.StyleManager = this.msmMain;
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Status: Stopped";
+            this.statusLabel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.statusLabel.UseStyleColors = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "http://i.imgur.com/iPkuNES.png";
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(219, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ImageLocation = "http://i.imgur.com/MgUJDaJ.png";
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 142);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Text = "EvoCity Radio Player";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 166);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.metroProgressSpinner1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.metroToggle1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Resizable = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.StyleManager = this.msmMain;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Components.MetroStyleManager msmMain;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel statusLabel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ EvoCity Radio Player/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvoCity_Radio_Player
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            metroProgressSpinner1.Visible = false;
+        }
+
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metroToggle1.Checked == true)
+            {
+                axWindowsMediaPlayer1.URL = @"http://51.254.200.243:8000/stream";
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsBuffering)
+            {
+                statusLabel.Text = "Status: Buffering";
+                metroProgressSpinner1.Visible = true;
+            }
+            else if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                statusLabel.Text = "Status: Playing";
+                metroProgressSpinner1.Visible = false;
+            }
+            else if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                statusLabel.Text = "Status: Stopped";
+                metroProgressSpinner1.Visible = false;
+            }
+        }
+
+
+        
+    }
+}
